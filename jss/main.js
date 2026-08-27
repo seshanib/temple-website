e3b4aa85be94eff27c0e335441fcad049d365cc9
Seshani <seshbalasooriya@gmail.com>
2026-08-27 21:55:20 +0530

    Dhamma Book Section

--- jss/main.js
+++ jss/main.js
@@ -5,3 +5,15 @@ window.addEventListener("load", function () {
         splash.classList.add("fade-out");
     }, 1500);
 });
+
+const booksContainer = document.querySelector(".books-grid");
+const booksScrollLeftBtn = document.querySelector(".books-scroll-left");
+const booksScrollRightBtn = document.querySelector(".books-scroll-right");
+
+booksScrollLeftBtn.addEventListener("click", function () {
+    booksContainer.scrollBy({ left: -250, behavior: "smooth" });
+});
+
+booksScrollRightBtn.addEventListener("click", function () {
+    booksContainer.scrollBy({ left: 250, behavior: "smooth" });
+});
